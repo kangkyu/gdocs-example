b13e5255b8cd6300dda84ef71a4506598fe293cd
Kang-Kyu Lee <kangkyu1111@gmail.com>
2023-08-27 22:30:10 -0700

    Broaden auth scope

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET'],
-    scope: 'email,profile,documents,documents.readonly'
+    scope: 'email,profile,documents,documents.readonly,spreadsheets,spreadsheets.readonly'
 end
 OmniAuth.config.allowed_request_methods = %i[post]
 # OmniAuth.config.logger = Rails.logger
